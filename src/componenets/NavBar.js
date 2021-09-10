3de22549c84e6c482fd135e202ef1d4959cfa78b
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-09-10 19:37:14 -0300

    Added NavBar component. Created some basic styling.

---
+++ src/componenets/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <img id="logoImg" src='../assets/logo.svg' alt='pokeball logo'></img>
+            <p id="logoText">Pokedex</p>
+        </nav>
+    );
+};
+
+export default NavBar;
