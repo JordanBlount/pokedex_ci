67470a8d326430ef17ce638c1fb6e8818c970d1e
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-09-10 22:12:40 -0300

    Finished setting up navbar + fixed logo svg

---
+++ refs/links.md
@@ -0,0 +1,3 @@
+https://blog.logrocket.com/how-to-use-svgs-in-react/
+
+https://fonts.google.com/specimen/Roboto
--- src/componenets/NavBar.js
+++ src/componenets/NavBar.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+// Imported SVG file so it would show up correctly. I couldn't place it directly in the img src
+import logo from '../assets/logo.svg'
+
 const NavBar = () => {
     return (
         <nav>
-            <img id="logoImg" src='../assets/logo.svg' alt='pokeball logo'></img>
+            <img id="logoImg" src={logo} alt='pokeball logo'></img>
             <p id="logoText">Pokedex</p>
         </nav>
     );
