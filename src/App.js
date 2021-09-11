675428f200c7e56effc4325713672b1a6519c436
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-09-10 21:26:17 -0300

    Updated favicon + app icons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './App.css'
+import NavBar from './componenets/NavBar';
 
 
 const App = () => {
   return (
     <div id="app">
-      
+      <NavBar />
     </div>
   );
 };
