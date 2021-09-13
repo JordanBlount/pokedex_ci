45628fab1879e5bb5e54aa9b2be35f67ac40b4d1
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-09-13 13:04:30 -0300

    Added methods to convert height and weight in the correct units.

--- src/componenets/Section.js
+++ src/componenets/Section.js
@@ -1,6 +1,19 @@
 import React from 'react';
 
+import convert from 'convert-units';
+
 const Section = (props) => {
+
+    // Converting height from decimeters to centimeters to inches
+    const setHeight = (height) => {
+        return convert((height * 10)).from('cm').to('in');
+    }
+
+    // Converting weight from hectograms to kilograms to pounds.
+    const setWeight = (weight) => {
+        return convert((weight * 0.1)).from('kg').to('lb');
+    }
+
     return (
         // TODO: Add some global styles to all of my sections. This is where I am going to put my spacing (container) logic so they line up correctly on mobile, tablet, and desktop.
         <div className="section">
@@ -9,11 +22,11 @@ const Section = (props) => {
                 {/* The "in-table" class is to help add additional styling to these "stat" divs. This style is being reused in places of my app. */}
                 <div className="stat in-table">
                     <p className="stat-title">Height</p>
-                    <p className="stat-data">{props.pokemonData.height}</p>
+                    <p className="stat-data">{`${setHeight(props.pokemonData.height).toFixed(0)} inches`}</p>
                 </div>
                 <div className="stat in-table">
                     <p className="stat-title">Weight</p>
-                    <p className="stat-data">{props.pokemonData.weight}</p>
+                    <p className="stat-data">{`${setWeight(props.pokemonData.weight).toFixed(0)} lbs`}</p>
                 </div>
             </div>
         </div>
--- wireframes/links.md
+++ wireframes/links.md
@@ -7,3 +7,4 @@ https://html-css-js.com/css/generator/box-shadow/
 https://pokemon.fandom.com/wiki/Types
 
 https://stackoverflow.com/questions/11726238/how-do-i-stop-an-image-displaying-outside-of-the-div
+
