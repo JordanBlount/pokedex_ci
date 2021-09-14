4ece4b424762f0718149fb078bd97cee102531b6
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2021-09-14 00:28:19 -0300

    Fixed number of pokemon

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ const App = () => {
     let text = search;
     if(!isNaN(text)) {
       let id = parseInt(text);
-      if(id > 1 && id < 1118) {
+      if(id > 1 && id < 898) {
         let name = pokemon.getName(parseInt(text));
         fetchPokemonData(name);
       } else {
