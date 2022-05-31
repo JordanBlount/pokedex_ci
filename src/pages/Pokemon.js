a16dc52c811d7f0a35f057e3a4ab549d94b525df
Jordan Blount <77266395+JordanBlount@users.noreply.github.com>
2022-05-31 15:19:22 -0400

    Removed unnecessary code and comments
    Removed code that would never be called under any circumstances.

--- src/pages/Pokemon.js
+++ src/pages/Pokemon.js
@@ -7,37 +7,17 @@ import Board from '../componenets/Board';
 
 const Pokemon = (props) => {
 
-    const navigate = useNavigate();
     const { id } = useParams();
 
     const pokemonData = useSelector(state => state.pokemonData);
 
+    // Handles loading pokemon if someone directly links to this page
     useEffect(() => {
         // Check to see if the state in App.js is default. If so, we fetch a new pokemon
         if(pokemonData.default) {
             // NOTE: Gets the pokemon based on the current id (we get this from React Router);
             props.submitSearch(null, id, false);
-        } else {
-            console.log("Got here");
-            // Checks to see if id is an integer
-            if(!Number.isNaN(id)) {
-                if(pokemonData.id === parseInt(id)) {
-                    // Nothing happens.... The page should not refresh.
-                } else {
-                    props.submitSearch(null, id, false);
-                }
-            } else if(typeof id === 'string') {
-                if(pokemonData.name.toLowerCase() === id.toLowerCase()) {
-                    // nothing happens...The page should not refresh.
-                } else {
-                    props.submitSearch(null, id.toLowerCase(), false);
-                }
-            } else {
-                // NOTE:
-                navigate('/');
-            }
         }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     
     return (
